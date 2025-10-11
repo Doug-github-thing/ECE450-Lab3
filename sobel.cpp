e93e4a620a561eae16d95050db98587aaf15bdf7
Doug-github-thing <dougrynar@gmail.com>
2025-10-11 01:02:12 -0400

    set up default design to test testbench developement

--- sobel.cpp
+++ sobel.cpp
@@ -1,6 +1,6 @@
 #include <ap_int.h>
 
-void compute_c (ap_int<8> in_bytes[512][512], ap_int<8> out_bytes[512][512])
+void sobel (ap_int<8> in_bytes[512][512], ap_int<8> out_bytes[512][512])
 {
     #pragma HLS INTERFACE ap_none port=in_bytes
     #pragma HLS INTERFACE ap_none port=out_bytes
