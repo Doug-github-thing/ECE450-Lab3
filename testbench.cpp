07202bae93a941c48fe22d8713a73b45f1b227d5
Doug-github-thing <dougrynar@gmail.com>
2025-10-11 16:50:57 -0400

    testbench reads greyscale ppms correctly

--- testbench.cpp
+++ testbench.cpp
@@ -4,9 +4,8 @@
 #include <array>
 #include <fstream>
 #include <memory>
-// #include "sobel.cpp"
 
-#define INPUT_PATH "/home/dog/school/fpgaece450/p3/sobel_in.ppm"
+#define INPUT_PATH "/home/dog/school/fpgaece450/p3/sobel_in_greyscale.ppm"
 #define OUTPUT_PATH "/home/dog/school/fpgaece450/p3/sobel_out_tb.ppm"
 #define WIDTH 512
 #define HEIGHT 512
@@ -14,11 +13,11 @@
 void sobel (ap_int<8> in_bytes[WIDTH][HEIGHT], ap_int<8> out_bytes[WIDTH][HEIGHT]);
 
 
-// Simple P5 (binary) PPM reader/writer for grayscale images
-bool read_ppm(const char* filename, uint8_t img[HEIGHT][WIDTH]) {
+// Simple PPM reader/writer
+bool read_ppm(const char* filename, ap_int<8> img[HEIGHT][WIDTH]) {
     std::ifstream file(filename, std::ios::binary);
     if (!file) {
-        std::cout << "File does not exist" << std::endl;
+        std::cout << "Input does not exist" << std::endl;
         return false;
     }
 
@@ -26,7 +25,7 @@ bool read_ppm(const char* filename, uint8_t img[HEIGHT][WIDTH]) {
     int w, h, maxval;
     file >> header >> w >> h >> maxval;
     file.ignore(1); // skip newline
-    if (header != "P6" || w != WIDTH || h != HEIGHT || maxval != 255) {
+    if (header != "P5" || w != WIDTH || h != HEIGHT || maxval != 255) {
         std::cout << "Incorrect file header" << std::endl;
         return false;
     }
@@ -36,15 +35,30 @@ bool read_ppm(const char* filename, uint8_t img[HEIGHT][WIDTH]) {
 }
 
 
+bool write_ppm(const char* filename, ap_int<8> img[HEIGHT][WIDTH]) {
+    std::ofstream file(filename, std::ios::binary);
+    if (!file) return false;
+
+    file << "P5 " << WIDTH << " " << HEIGHT << " 255\n";
+    file.write(reinterpret_cast<char*>(img), WIDTH * HEIGHT);
+    return true;
+}
+
+
 int main() {
 
 
     std::cout << "!!!! Starting test !!!!" << std::endl;
     
-    uint8_t in_bytes[512][512];
-    uint8_t out_bytes[512][512];
+    ap_int<8> in_bytes[512][512];
+    // ap_int<8> out_bytes[512][512];
     
     if(!read_ppm(INPUT_PATH, in_bytes))
+        return -1;
+
+    // sobel(in_bytes, &out_bytes);
+    
+    if(!write_ppm(OUTPUT_PATH, in_bytes))
         return -1;
 
     std::cout << ".... Finshing test ...." << std::endl;
