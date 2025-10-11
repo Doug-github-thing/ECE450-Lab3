f04e27751bbc3be98c72c48bb9b9666097311461
Doug-github-thing <dougrynar@gmail.com>
2025-10-11 18:38:58 -0400

    not optimized yet, but finally produces an edge detected output

--- sobel.cpp
+++ sobel.cpp
@@ -1,18 +1,63 @@
 #include <ap_int.h>
 
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+
 #define WIDTH 512
 #define HEIGHT 512
+/**
+ * Sobel flowchart:
+ * 
+ * Define sobel kernels
+ * 
+ * Iterate through every pixel except for the image edges:
+ * for each pixel:
+ *    Populate Gx[at that pixel] by applying sobel operator on left and right sides:
+ *        Gx[pixel] = [1,2,1] applied to the right - [1,2,1] applied to the left
+ *    Populate Gy[at that pixel] by applying sobel operator above and below:
+ *        Gy[pixel] = [1,2,1] applied to below - [1,2,1] applied to above
+ 
+ *    G = sqrt(Gx^2 + Gy^2)
+ * 
+ *    Normalize values to 255 scale
+ */
 
-void sobel (ap_int<8> in_bytes[WIDTH][HEIGHT], ap_int<8> out_bytes[WIDTH][HEIGHT])
+void sobel (ap_int<8> in[WIDTH][HEIGHT], ap_int<8> out[WIDTH][HEIGHT])
 {
-    #pragma HLS INTERFACE ap_none port=in_bytes
-    #pragma HLS INTERFACE ap_none port=out_bytes
+    #pragma HLS INTERFACE ap_none port=in
+    #pragma HLS INTERFACE ap_none port=out
     #pragma HLS INTERFACE ap_ctrl_none port=return
 
+    // Define Sobel kernels.
+    // Typically they're shown as 3x3 matrix with 0s in the middle. That's wasted space.
+    const ap_int<2> sobel[] = {1, 2, 1};    
+
+    ap_int<9> G[WIDTH][HEIGHT];
+
     // Simple starter code for testing the testbench before implementation of filter begins
-    RowsLoop: for (int i=0; i<HEIGHT; ++i) {
-        ColsLoop: for (int j=0; j<WIDTH; ++j) {
-            out_bytes[i][j] = in_bytes[i][j];
+    RowsLoop: for (int i=1; i<HEIGHT-1; ++i) {
+        ColsLoop: for (int j=1; j<WIDTH-1; ++j) {
+
+            // Max possible value happens when right and below pixel values are 255 and left and above pixels are 0.
+            // The result is Gx = Gy = 255+2*255+255 = 1020. Only requires 10 bits
+            // Final G value is G = sqrt(2*1020^2) = 1443
+
+            // Sobel kernel (1,2,1) to the left and right, subtracted
+            int Gx = (in[i-1][j+1])+(2*in[i][j+1])+(in[i+1][j+1])
+                   - ((in[i-1][j-1])+(2*in[i][j-1])+(in[i+1][j-1]));
+            
+            // Sobel kernel (1,2,1) to the below and top, subtracted
+            int Gy = (in[i+1][j-1])+(2*in[i+1][j])+(in[i+1][j+1]) 
+                   - ((in[i-1][j-1])+(2*in[i-1][j])+(in[i-1][j+1]));
+
+            // // Hypotenuse between x and y axis
+            int G = sqrtf(Gx*Gx + Gy*Gy);
+
+            // // Normalize back to 255 range
+            out[i][j] = (G  * 255 / 1443);
         }
     }
     
