12fccaeec135eb16f09a90fd34c9ad71f7533ac6
Doug-github-thing <dougrynar@gmail.com>
2025-10-13 13:48:10 -0400

    moved stream write after processing into the main loop to simplify testbench integration

--- sobel.cpp
+++ sobel.cpp
@@ -2,7 +2,6 @@
 #include <hls_math.h>
 #include <hls_stream.h>
 
-
 /**
  * Sobel flowchart:
  * 
@@ -22,54 +21,46 @@
 
 void sobel (hls::stream<ap_uint<8>> &in_stream, hls::stream<ap_uint<8>> &out_stream)
 {
+    // Stream the data bytes in and out
     #pragma HLS INTERFACE axis port=in_stream
     #pragma HLS INTERFACE axis port=out_stream
-    #pragma HLS INTERFACE ap_ctrl_none port=return
-
+    
     ap_uint<8> in[HEIGHT][WIDTH];
-    ap_uint<8> out[HEIGHT][WIDTH];
-
-    RowsLoopInit: for (ap_uint<10> i=0; i<HEIGHT; ++i) {
-        ColsLoopCalcInit: for (ap_uint<10> j=0; j<WIDTH; ++j) {
+    
+    // Read input from stream directly to internal array for parallelizable processing later
+    InitialRowLoop: for (ap_uint<10> i=0; i<HEIGHT; ++i) {
+        InitialColLoop: for (ap_uint<10> j=0; j<WIDTH; ++j) {
             in[i][j] = in_stream.read();
         }
     }
 
-    // Define Sobel kernels.
-    // Typically they're shown as 3x3 matrix with 0s in the middle. That's wasted space.
-    
-    RowsLoopCalc: for (ap_uint<10> i=1; i<HEIGHT-1; ++i) {
-        ColsLoopCalc: for (ap_uint<10> j=1; j<WIDTH-1; ++j) {
-            #pragma HLS PIPELINE II=1
-            #pragma HLS UNROLL factor=4
-
-            // Sobel kernel (1,2,1) to the left and right, subtracted
-            ap_uint<10> right_side = (in[i-1][j+1])+(2*in[i][j+1])+(in[i+1][j+1]); 
-            ap_uint<10> left_side =  (in[i-1][j-1])+(2*in[i][j-1])+(in[i+1][j-1]); 
-            ap_int<11> Gx = right_side - left_side;
+    // Process AND write immediately to prevent lockup during cosim
+    ProcessingRowLoop: for (ap_uint<10> i=0; i<HEIGHT; ++i) {
+        ProcessingColLoop: for (ap_uint<10> j=0; j<WIDTH; ++j) {
+            ap_uint<8> out_pixel = 0;
             
-            // Sobel kernel (1,2,1) to the below and top, subtracted
-            ap_uint<10> bottom_side = (in[i+1][j-1])+(2*in[i+1][j])+(in[i+1][j+1]);
-            ap_uint<10> top_side = ((in[i-1][j-1])+(2*in[i-1][j])+(in[i-1][j+1]));
-            ap_int<11> Gy = bottom_side - top_side;
+            // if logic is to restrict sobel operator for use not on the outer square.
+            // Needed since the loop iterates over the whole image instead of just the inside square.
+            if (i>0 && i<HEIGHT-1 && j>0 && j<WIDTH-1) {
+                // Your exact Sobel calculation here
+                ap_uint<10> right_side = (in[i-1][j+1])+(2*in[i][j+1])+(in[i+1][j+1]); 
+                ap_uint<10> left_side =  (in[i-1][j-1])+(2*in[i][j-1])+(in[i+1][j-1]); 
+                ap_int<11> Gx = right_side - left_side;
+                
+                ap_uint<10> bottom_side = (in[i+1][j-1])+(2*in[i+1][j])+(in[i+1][j+1]);
+                ap_uint<10> top_side = ((in[i-1][j-1])+(2*in[i-1][j])+(in[i-1][j+1]));
+                ap_int<11> Gy = bottom_side - top_side;
 
-            // Use approximation since exact sqrt value is not vital for edge detection 
-            // Use G ~= 0.707 * (|Gx| + |Gy|) instead of the slow pythagorean calculation
-            ap_uint<11> absGx = hls::abs(Gx);
-            ap_uint<11> absGy = hls::abs(Gy);
-            // Multiply by 181 and divide by 256 instead of 0.707, makes cleaner int operation
-            ap_uint<12> G = (absGx + absGy) * 181 >> 8;
+                ap_uint<11> absGx = hls::abs(Gx);
+                ap_uint<11> absGy = hls::abs(Gy);
+                ap_uint<12> G = (absGx + absGy) * 181 >> 8;
 
-            // Normalize back to 255 range, clamping max values at a chosen value
-            if (G >= NORMALIZATION_FACTOR)
-                G = 255;
-            out[i][j] = (G  * 255 / NORMALIZATION_FACTOR);
-        }
-    }
-
-    RowsLoopFinal: for (ap_uint<10> i=0; i<HEIGHT; ++i) {
-        ColsLoopCalcFinal: for (ap_uint<10> j=0; j<WIDTH; ++j) {
-            out_stream.write(out[i][j]);
+                if (G >= NORMALIZATION_FACTOR)
+                    G = 255;
+                out_pixel = (G * 255 / NORMALIZATION_FACTOR);
+            }
+            
+            out_stream.write(out_pixel);
         }
     }
 }
